ed9f6b6b670f1ad27706a3899b03c7b839df8227
wirew0lf <wirew0lf@wirew0lf.com>
2024-04-10 17:24:32 +0200

    Give allowance to bridge and add connext function

--- src/BridgeEscrow.sol
+++ src/BridgeEscrow.sol
@@ -5,10 +5,15 @@ import {EscrowBase} from "./EscrowBase.sol";
 
 import {IL1GatewayRouter} from "token-bridge-contracts/tokenbridge/ethereum/gateway/IL1GatewayRouter.sol";
 
+import {IERC20} from "openzeppelin-contracts/interfaces/IERC20.sol";
+import {SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+
 /// @title Bracket's Escrow Contract
 /// @author Bracket.fi
 /// @dev L1 Escrow with capabilities for bridging the tokens to Arbitrum at the end of escrow.
 contract BridgeEscrow is EscrowBase {
+    using SafeERC20 for IERC20;
+
     IL1GatewayRouter public bridgeRouter;
 
     constructor() {
@@ -24,11 +29,18 @@ contract BridgeEscrow is EscrowBase {
         bridgeRouter = IL1GatewayRouter(router);
     }
 
-    function bridgeToken(address token, address arbEscrow, uint256 amount, uint256 maxGas, uint256 gasPrice)
+    function bridgeTokenArb(address token, address arbEscrow, uint256 amount, uint256 maxGas, uint256 gasPrice)
         public
         onlyOwner
         onlyBroke
     {
+        IERC20(token).safeIncreaseAllowance(address(bridgeRouter), amount);
         bridgeRouter.outboundTransferCustomRefund(token, msg.sender, arbEscrow, amount, maxGas, gasPrice, bytes(""));
     }
+
+    function bridgeTokenConnext(address token, address arbEscrow, uint256 amount, uint256 maxGas, uint256 gasPrice)
+        public
+        onlyOwner
+        onlyBroke
+    {}
 }
